65e05bf749b02511873c1662ae7883984723df54
jaflesch <jean.flesch93@gmail.com>
2018-10-28 14:09:42 -0300

    Adicionar filtros de pesquisa por usuário e vaga #76

--- src/_controller/pesquisar.php
+++ src/_controller/pesquisar.php
@@ -6,16 +6,18 @@ class Pesquisar extends Controller {
 		$value = $this->get->search;
 		$area = isset($this->get->area) ? $this->get->area : "";
 
+		Search::store($value);
+
+		// Prepare bag
 		if($area == "usuario") {
-			$bag['search'] = Search::getAllFromValue($value);
+			$bag['search'] = Search::getUserFromValue($value);
 		}
 		else if($area == "vaga") {
 			$bag['search'] = Search::getJobFromValue($value);
 		}
 		else {
-			$bag['search'] = Search::getUserFromValue($value);	
-		}
-		
+			$bag['search'] = Search::getAllFromValue($value);	
+		}		
 		$bag['related_searchs'] = Search::getRelated();
 		
 		$this->render("pesquisar/index", $bag);
--- src/_model/Search.php
+++ src/_model/Search.php
@@ -1,10 +1,30 @@
 <?php
 class Search {
 	
+	public static function store($value) {
+		$db = new DBConn();
+		
+		$r = $db->query("SELECT id FROM search WHERE text = '{$value}'");
+
+		if($r == NULL) {
+			return $db->insert("INSERT INTO search (text) VALUES ('{$value}')");
+		}
+		else {
+			return $db->update("
+				UPDATE search 
+				SET
+					datetime_last_search = NOW(),
+					total = total + 1
+				WHERE text = '{$value}'
+			");
+		}
+	}
+
 	public static function getAllFromValue($value) {
 		$db = new DBConn();
+		$value = utf8_decode($value);
 
-		return $db->query("
+		$result = $db->query("
 			(
 				SELECT id, title, 'job' AS category, '' AS login
 				FROM job 
@@ -16,7 +36,18 @@ class Search {
 				FROM user
 				WHERE active = 1 AND (name LIKE '%{$value}%' OR login LIKE '%{$value}%')
 			)
+			ORDER BY title
 		", true);
+
+		if(is_array($result)) {
+			foreach ($result as &$r) {
+				if($r->category == "job") {
+					$r->slug = linkfy(trim($r->title));
+				}
+			}			
+		}
+
+		return $result;
 	}
 
 	public static function getJobFromValue($value) {
@@ -26,6 +57,7 @@ class Search {
 			SELECT id, title, 'job' AS category, '' AS login
 			FROM job 
 			WHERE active = 1 AND (title LIKE '%{$value}%' OR resume LIKE '%{$value}%' OR text LIKE '%{$value}%')
+			ORDER BY title
 		", true);
 	}
 
@@ -36,10 +68,18 @@ class Search {
 			SELECT id, name AS title, 'user' AS category, login
 			FROM user
 			WHERE active = 1 AND (name LIKE '%{$value}%' OR login LIKE '%{$value}%')
+			ORDER BY name
 		", true);
 	}
 
 	public static function getRelated() {
-		return true;
+		$db = new DBConn();
+
+		return $db->query("
+			SELECT text 
+			FROM search 
+			ORDER BY total DESC, datetime_last_search DESC
+			LIMIT 10
+		");
 	}
 }
