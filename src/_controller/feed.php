970a3371a75547816010cd212b9954b5972273f7
jaflesch <jean.flesch93@gmail.com>
2018-11-03 14:50:01 -0300

    Limitar uma recomendação por usuário em cada perfil

--- src/_controller/feed.php
+++ src/_controller/feed.php
@@ -13,6 +13,7 @@ class Feed extends Controller {
 		$bag['followers'] = Follow::getAllFollowers($bag['profile']['user']->id);
 		$bag['recomendations'] = RecommendUser::getAllFromUserId($bag['profile']['user']->id);
 		$bag['user_is_blocked_by_me'] = Block::isUserInMyBlockedList($login);
+		$bag['has_recommended'] = RecommendUser::hasRecommend($login);
 		
 		$this->render("feed/index", $bag);
 	}
--- src/_model/RecommendUser.php
+++ src/_model/RecommendUser.php
@@ -6,7 +6,7 @@ class RecommendUser {
 		return $db->query("
 			SELECT 
 				ru.*, 
-				u.name user_name, u.login user_login, u.id user_id, u.avatar user_avatar,
+				u.name user_name, u.login user_login, u.id user_id, u.avatar user_avatar, u.gender user_gender,
 				u.born_in_city user_born_in_city, u.born_in_state user_born_in_state, 
 				u.live_in_city user_live_in_city, u.live_in_state user_live_in_state,
 				uj.title user_job_title, 
@@ -24,9 +24,38 @@ class RecommendUser {
 		$db = new DBConn();
 		$id_user = Auth::id();
 
-		return $db->insert("
-			INSERT INTO recommendation_user (id_user, id_target, text, datetime_created, datetime_last_edit)
-			VALUES ({$id_user}, {$post->user_id}, '{$post->text}', NOW(), NOW())
+		$r = $db->query(
+			"SELECT id 
+			FROM recommendation_user 
+			WHERE id_user = {$id_user} AND id_target = {$post->user_id} AND active = 1
+		");
+
+		if($r == NULL) {
+			return $db->insert("
+				INSERT INTO recommendation_user (id_user, id_target, text, datetime_created, datetime_last_edit)
+				VALUES ({$id_user}, {$post->user_id}, '{$post->text}', NOW(), NOW())
+			");			
+		}
+		else {
+			return $db->insert("
+				UPDATE recommendation_user 
+				SET 
+					text = '{$post->text}',
+					datetime_last_edit = NOW()
+				WHERE id_user = {$id_user} AND id_target = {$post->user_id} AND active = 1
+			");				
+		}
+	}
+
+	public static function hasRecommend($user_login) {
+		$db = new DBConn();
+		$id_user = Auth::id();
+
+		return $db->query("
+			SELECT *
+			FROM recommendation_user ru
+			INNER JOIN user u ON ru.id_target = u.id AND u.login = '{$user_login}'
+			WHERE id_user = {$id_user} AND ru.active = 1
 		");
 	}
 }
