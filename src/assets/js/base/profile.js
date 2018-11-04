c390097893aab19df9640e12df43297b84b39db6
jaflesch <jean.flesch93@gmail.com>
2018-11-04 10:31:16 -0200

    Corrigir bug de seguir usuários #110

--- src/assets/js/base/profile.js
+++ src/assets/js/base/profile.js
@@ -409,4 +409,26 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+	// Follow
+	$(".btn-follow").click(function(e) {
+		var el = $(this);
+		var id = $(this).data("id");
+		
+		if(el.hasClass('following')) {
+			el.text('Seguir');
+			el.removeClass('following');
+		}
+		else {
+			el.text('Seguindo');
+			el.addClass('following');	
+		}
+
+		$.ajax({
+			url: !el.hasClass('following') ? './../../feed/deixar-seguir' : './../../feed/seguir',
+			method: 'POST',
+			dataType: 'json',
+			data: { id: id }
+		});
+	});
 });
